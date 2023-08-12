90bf0e525b262143d9cd7f635c91713c4b2061c9
Scott <scott.crisp@oneadvanced.com>
2023-08-12 11:33:04 +0100

    fixed bug w/ environment variables

--- src/app/services/feedback.service.ts
+++ src/app/services/feedback.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 
 import { map } from 'rxjs';
 
-import { environment } from 'src/environments/environment.dev';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
