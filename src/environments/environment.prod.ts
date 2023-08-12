bd0401b0ff57a86c802aa78ce21e765723423677
Scott <scott.crisp@oneadvanced.com>
2023-08-12 10:29:00 +0100

    updating for heroku backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  defaultUrl: 'http://scottcrisp.uk/', // TOM BE ADDED
-  gatewayUrl: 'http://api.scottcrisp.uk/' // TO BE ADDED
+  defaultUrl: 'http://scottcrisp.uk/',
+  gatewayUrl: 'http://sc-heroku-portfolio-api-cb2e005ecc5e.herokuapp.com/'
 }
